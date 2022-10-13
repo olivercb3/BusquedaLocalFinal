0fdbd27756b158254910e061376693285e47aa6d
pau dorca oliver <pau.dorca@estudiantat.upc.edu>
2022-10-13 15:38:04 +0100

    successor function creada correctament

---
+++ Prac1_IA/src/prac1_ia/SuccesorFunctionEnergy.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prac1_ia;
+
+import aima.search.framework.SuccessorFunction;
+import java.util.List;
+
+public class SuccesorFunctionEnergy implements SuccessorFunction {
+
+    @Override
+    public List getSuccessors(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
