58e3945c2b756c725ed06b137b59d8e3f76f9a5e
olivercb3 <47102398+olivercb3@users.noreply.github.com>
2022-10-15 23:47:33 +0200

    canvi en la estructura del estat inicial

--- Prac1_IA/src/prac1_ia/Practica.java
+++ Prac1_IA/src/prac1_ia/Practica.java
@@ -4,6 +4,7 @@
  */
 package prac1_ia;
 import IA.Energia.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,29 +16,30 @@ public class Practica {
      private static Centrales centrales; 
      private static Clientes clientes; 
      private static VEnergia venergia;
-     private static int[][] table; 
+     private static ArrayList<ArrayList<Integer>> table;
+     private static ArrayList<Double> ProduccioRestant;
+     
      
      public static void InitialState() { //estat inicial on s'assignen els clients a les centrals a mesura que es van omplint.
         int indice_cliente = 0;
         for (int i = 0; i < centrales.size(); ++i) {
             boolean max_superat = false;
-            double produccion_restante = centrales.get(i).getProduccion();
+            double produccion_restante = ProduccioRestant.get(i);
             for (int j = indice_cliente; j < clientes.size() && max_superat == false; ++j) {
                 if (clientes.get(j).getConsumo() < produccion_restante) {
-                    table[0][indice_cliente] = i;
-                    table[1][indice_cliente] = j;
+                    table.get(i).add(j);
                     produccion_restante -= clientes.get(j).getConsumo();
-                    ++indice_cliente;tt
+                    ++indice_cliente;
                 }
                 else {
                     max_superat = true;
                 }
+                ProduccioRestant.set(i, produccion_restante);
             }
         }
         if (indice_cliente < clientes.size()) {
             for (int i = indice_cliente; i < clientes.size(); ++i) {
-                table[0][indice_cliente] = centrales.size();
-                table[1][indice_cliente] = i;
+                table.get(centrales.size()).add(i);
             }
         }
      }
@@ -47,13 +49,27 @@ public class Practica {
         
         int[] ia = {10,20,15}; 
         double[] cl = {0.2, 0.3, 0.5}; 
+        
         centrales = new Centrales(ia,1);
         clientes = new Clientes(600, cl, 0.5, 291200) ;
-        table = new int[2][clientes.size()];
+        
+        table = new ArrayList<>(centrales.size()+1);
+        ProduccioRestant = new ArrayList<>();
+        
+        for(int i=0; i < clientes.size(); i++) {
+            table.add(new ArrayList());
+        }
+        for(int i=0; i < centrales.size(); i++) {
+            ProduccioRestant.add(centrales.get(i).getProduccion()); 
+        }
+        
         InitialState();
-        for (int i = 0; i < table.length; ++i) {
-            for (int j = 0; j < table[0].length; ++j) {
-                System.out.print(table[i][j] + " ");
+        
+        
+        for (int i = 0; i < table.size(); ++i) {
+            System.out.print(i + ": ");
+            for (int j = 0; j < table.get(i).size(); ++j) {
+                System.out.print(table.get(i).get(j) + " ");
             }
             System.out.println();
         }
