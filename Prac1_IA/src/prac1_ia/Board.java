aa365fe96890d984a36ddb29586d6299dcb58830
olivercb3 <47102398+olivercb3@users.noreply.github.com>
2022-10-16 10:44:10 +0200

    classe del estat

--- Prac1_IA/src/prac1_ia/Board.java
+++ Prac1_IA/src/prac1_ia/Board.java
@@ -1,9 +1,53 @@
-package prac1_ia;/*
+package prac1_ia;
+
+import IA.Energia.Clientes;
+import IA.Energia.Centrales;
+import java.util.ArrayList;
+
+
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
 public class Board {
 
-    private static int[][] table;
+    private static DistanceCalculator distanceCalculator;
+
+    /**
+     * List with all the clients
+     */
+    private static Clientes clientes;
+    /**
+     * List with all the power centers
+     */
+    private static Centrales centrales;
+    /**
+     * All Client to power Centers Assignments
+     */
+    private static ArrayList<Integer> assignacions;
+
+    /**
+     * Array with the production left for each power Center
+     */
+    private ArrayList<Integer> ProduccioRestant;
+
+    /**
+     * Constructor for an empty TruckSchedule
+     * @param dc Support structure representing all distribution centers
+     * @param gs Support structure representing all gas stations
+     * @param p Support structure representing all petitions
+     * Creates an empty truck schedule
+     */
+    public Board(Centrales cen, Clientes cl, ArrayList<Integer> assig, ArrayList<Integer> prod_res) {
+        centrales = cen;
+        clientes = cl;
+        assignacions = assig;
+        ProduccioRestant = prod_res;
+        initialize();
+    }
+    
+    private void initialize(){
+        distanceCalculator = DistanceCalculator.getInstance(this);
+    }
 }
--- Prac1_IA/src/prac1_ia/Practica.java
+++ Prac1_IA/src/prac1_ia/Practica.java
@@ -134,4 +134,8 @@ public class Practica {
             System.out.println(action);
         }
     }
+    
+    public static void runSimulation() {
+        //
+    }
 }
